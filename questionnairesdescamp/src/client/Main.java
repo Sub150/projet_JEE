e167573cd70126220e0460e2e95db178756d21a1
Simon Descamps <simon.descamps.etu@univ-lille.fr>
2020-03-18 12:31:48 +0100

    1st commit

---
+++ questionnairesdescamp/src/client/Main.java
@@ -0,0 +1,16 @@
+package client ;
+
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException ;
+
+
+
+
+public class Main {
+	
+	
+ public static void main(String[] args) { 
+ }  
+}
+
